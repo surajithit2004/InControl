bd778accf22ce3401f56d291222c2e60d6bd00e8
Surajit De <surajithit2004@gmail.com>
2020-04-17 15:04:42 +0200

    InControl Project
    InControl for ComplianceWise on 17 April 2020

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RiskServiceService } from './risk-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'InControl';
+  errorMessage = '';
+  riskLevels: any[]= [];
+  risks: any[] = [];
+
+  constructor(private riskServiceService: RiskServiceService) { }
+
+  // ngOnInit(): void {
+  //   this.riskServiceService.getAllRisks().subscribe({
+  //     next: risks => {
+  //       this.riskLevels = this.risks;
+  //       console.log("riskLevels = "+this.riskLevels);
+  //     },
+  //     error: err => this.errorMessage = err
+  //   });
+  // }
+
+  ngOnInit(): void {
+    this.riskLevels = [
+        {
+          "id": 1,
+          "chamberOfCommerceNumber": 1001,
+          "riskLevel": "Low"
+        },
+        {
+          "id": 2,
+          "chamberOfCommerceNumber": 2001,
+          "riskLevel": "Medium"
+        },
+        {
+          "id": 3,
+          "chamberOfCommerceNumber": 3001,
+          "riskLevel": "High"
+        },
+        {
+          "id": 4,
+          "chamberOfCommerceNumber": 4001,
+          "riskLevel": "Medium"
+        }
+      ];
+
+      
+  }
+  
 }
---
+++ src/app/risk-service.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RiskServiceService {
+
+  private risksUrl = 'http://localhost:3000/risks';
+
+  constructor(private http: HttpClient) { }
+
+  getAllRisks(): Observable<any[]> {
+    return this.http.get<any[]>(this.risksUrl)
+      .pipe(
+        tap(data => console.log('All: ' + JSON.stringify(data))),
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
+}
